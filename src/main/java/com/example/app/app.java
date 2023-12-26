76ecc7900d58eec008fca092841a2b7279eb06d0
Rita Haimov <shlomihaimov1@gmail.com>
2023-12-26 20:43:16 +0200

    app structre

---
+++ src/main/java/com/example/app/app.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(app.class, args);
+	}
+}
